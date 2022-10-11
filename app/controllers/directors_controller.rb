e809b4c6b1306bbbee72d8c581a0a81aa4054fe0
mpuchyr <matt.puchyr@gmail.com>
2022-10-11 16:13:52 +0000

    Get youngest and eldest director pages and routes working

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -4,4 +4,18 @@ class DirectorsController < ApplicationController
 
     render({ :template => "director_templates/index.html.erb"})
   end
+
+  def eldest
+    sorted_directors = Director.all.order({ :dob => :asc }).where.not({ :dob => nil })
+    @director = sorted_directors.at(0)
+    @dob = @director.dob.strftime("%B %d, %Y")
+    render({ :template => "director_templates/eldest.html.erb"})
+  end
+
+  def youngest
+    sorted_directors = Director.all.order({ :dob => :desc}).where.not({ :dob => nil})
+    @director = sorted_directors.at(0)
+    @dob = @director.dob.strftime("%B %d, %Y")
+    render({ :template => "director_templates/youngest.html.erb"})
+  end
 end
