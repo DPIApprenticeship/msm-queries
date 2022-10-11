250579fec78943cc5659acc77bff33aa1f64cf1a
mpuchyr <matt.puchyr@gmail.com>
2022-10-11 19:38:02 +0000

    Complete movie routes and controller

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,14 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+
+    render({ :template => "movie_templates/index.html.erb"})
+  end
+
+  def movie
+    @id = params.fetch(:id)
+    @movie = Movie.where({ :id => @id}).at(0)
+    @director = Director.where({ :id => @movie.director_id}).at(0).name
+    render({ :template => "movie_templates/movie.html.erb"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   get("/directors/eldest", { :controller => "directors", :action => "eldest"})
   get("/directors/youngest", { :controller => "directors", :action => "youngest"})
   get("/directors/:id", { :controller => "directors", :action => "director_info"})
+
+  get("/movies", { :controller => "movies", :action => "index"})
+  get("/movies/:id", { :controller => "movies", :action => "movie"})
+
+  get("/actors", { :controller => "actors", :action => "index"})
 end
